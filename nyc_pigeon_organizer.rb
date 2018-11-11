d855f730a0bf61e24194f78c8374575224b064ca
Adam Saland <a.jpagnano@gmail.com>
2018-11-11 17:02:10 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, hsh), res|
+    hsh.each do |values,names|
+      names.each do |name| 
+        res[name] ||= {}
+        res[name][key] ||= []
+        res[name][key] << value.to_s
+      end
+    end
+  end
 end
