25c579f20b4bf22c6d888885c1832ec2ec5fff0e
Adam Saland <a.jpagnano@gmail.com>
2018-11-11 17:03:48 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.each_with_object({}) do |(key, hsh), res|
-    hsh.each do |values,names|
-      names.each do |name| 
-        res[name] ||= {}
-        res[name][key] ||= []
-        res[name][key] << value.to_s
+  new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
+        end
       end
     end
   end
+  new_hash
 end
